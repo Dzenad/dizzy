e42812eb9e05bb9747bfd90fc37f6f75fa7289b1
David Ly <dly5891@gmail.com>
2017-03-23 17:18:39 -0500

    setting up node files

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const path = require('path');
 const open = require('open');
 const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const api = require('./server/routes/api');
 
 const app = express();
-const CONFIG = require('./localcfg.json');
-const PORT = CONFIG.LOCAL_PORT;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/api', api);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/src/app/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-app.listen(3000, (err) => {
+const port = process.env.PORT || '3000';
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, (err) => {
   if (err) {
     console.error(err);
   }
