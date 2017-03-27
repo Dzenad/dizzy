afe32988f0237451184137ee3b3caf495cef7fb8
David Ly <dly5891@gmail.com>
2017-03-27 17:25:04 -0500

    made a mistake on server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 
-const api = require('./server/routes/api');
 
 const app = express();
 
@@ -13,7 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/api', api);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
